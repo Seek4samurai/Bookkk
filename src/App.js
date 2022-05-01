cc5b656e3117cc83810d6704fc93eeb971423f81
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-05-01 11:53:28 +0530

    feat: recover password

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import ForgetPassword from "./components/ForgetPassword";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import ResetPassword from "./components/ResetPassword";
 import Signup from "./components/Signup";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route exact path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { account } from "../services/appwriteConfig";
 import "react-toastify/dist/ReactToastify.css";
 
 const Signup = () => {
-  const navigate = useNavigate();
   const [userDetails, setUserDetails] = useState({
     name: "",
     email: "",
@@ -20,7 +19,6 @@ const Signup = () => {
         userDetails.password
       );
       await account.createSession(userDetails.email, userDetails.password);
-      navigate("/home");
     } catch (error) {
       console.log(error);
     }
