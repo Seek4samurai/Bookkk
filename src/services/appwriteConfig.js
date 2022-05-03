046785d31fa63cce664d0c9fb795d0741088b0a5
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-05-03 08:37:26 +0530

    fix: sdk endpoint

--- src/services/appwriteConfig.js
+++ src/services/appwriteConfig.js
@@ -3,7 +3,7 @@ import { Appwrite } from "appwrite";
 const sdk = new Appwrite();
 
 sdk
-  .setEndpoint("http://localhost/signup/v1")
+  .setEndpoint("https://localhost/v1")
   .setProject(process.env.REACT_APP_PROJECT_ID);
 
 export const account = sdk.account;
